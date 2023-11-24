3c4e59bae623d5caa28e51b14cbb9f31a426673a
lesquive <luis.jose.esquivel@gmail.com>
2023-11-24 15:05:37 -0600

    added core project structure

---
+++ LasPisadas/LasPisadas/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using LasPisadas.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000", "http://localhost:7279")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddDbContext<LasPisadasDbContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("LasPisadasDbContext"), new MySqlServerVersion(new Version(8, 0, 25)));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowLocalhost");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
